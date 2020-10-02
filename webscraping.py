658725d348bd542af3ebb9dc75a3ab945e09ca39
Albert Wiryawan <albert.vwirya360@gmail.com>
2020-10-02 13:08:03 -0500

    almost finished

---
+++ webscraping.py
@@ -0,0 +1,48 @@
+# This script pertains to the scraping of MSU technologies
+import requests
+from bs4 import BeautifulSoup
+
+# check to make sure that the website link is good 
+result = requests.get('http://msut.technologypublisher.com/searchresults.aspx?q=pharmaceutical')
+# print(result.status_code)
+
+#puts all the html into src variable
+src= result.content
+# print(src)
+
+#create beautiful soup object in order to add more functionality to content variable 
+soup = BeautifulSoup(src,'lxml')
+
+#python list to store all URL headers
+atag = []
+for h2_tag in soup.find_all("h2"):
+    a_tag = h2_tag.find('a', href=True)
+    atag.append(a_tag)
+
+# gives all the urls in a python list with each element stored as strings 
+urls = []
+for i in atag:
+    if i is not None:
+        urls.append(i['href'])       
+
+base_url = 'http://msut.technologypublisher.com/'
+
+#create final list of links
+final_urls = []
+for i in urls: 
+    final_string = "".join((base_url, i))
+    final_urls.append(final_string)
+
+###################################################################################################
+#After creating a means to get all the final urls create a final loop to make a request for every page and save the html
+
+description = []
+for i in final_urls:
+    res = requests.get(i)
+    source = res.content
+    final_soup = BeautifulSoup(src,'lxml')
+    print(final_soup)
+    des = final_soup.find_all("div", {"class": "c_tp_description"})
+    description.append(des)
+
+# print(description)
